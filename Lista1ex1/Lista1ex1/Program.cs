d72d9fd0381eefe2d4f721f2bd02d476ffd5f674
Guimascena <gmascena.odm@gmail.com>
2026-04-09 22:13:56 -0300

    Add files via upload

---
+++ Lista1ex1/Lista1ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+
+            int h;
+
+            int a;
+
+            Console.WriteLine("Informe  a base do retângulo:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura do retângulo:");
+            h = int.Parse(Console.ReadLine());
+
+            a = b * h;
+
+            Console.WriteLine("A area do retângulo é:");
+            Console.WriteLine(a);
+        }
+    }
+}
