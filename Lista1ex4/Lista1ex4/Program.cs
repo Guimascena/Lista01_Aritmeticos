b68a0c866d8a8cb9d544e675bd9c292ba4a2dc21
Guimascena <gmascena.odm@gmail.com>
2026-04-09 23:37:04 -0300

    Add files via upload

---
+++ Lista1ex4/Lista1ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+
+            Console.WriteLine("Digite o valor da base do triângulo:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura do triângulo:");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do triângulo é {0}", (b*a)/2);
+        }
+    }
+}
