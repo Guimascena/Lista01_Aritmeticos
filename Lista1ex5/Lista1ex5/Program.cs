3826731090a9555b1fe02d88dd84efa4d15c404a
Guimascena <gmascena.odm@gmail.com>
2026-04-10 00:17:59 -0300

    Add files via upload

---
+++ Lista1ex5/Lista1ex5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            int i = 1;
+
+            while (i < 5)
+            {
+                Console.WriteLine("Digite a nota {0}:", i);
+                soma += double.Parse(Console.ReadLine());
+                i++;
+            }
+
+            Console.WriteLine("A média é {0}", soma / 4);
+        }
+    }
+}
