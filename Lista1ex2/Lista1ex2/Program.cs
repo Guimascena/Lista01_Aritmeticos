5c2dcfc8149d0606d2cf02b3157f7bc134163bcf
Guimascena <gmascena.odm@gmail.com>
2026-04-09 22:33:02 -0300

    Add files via upload

---
+++ Lista1ex2/Lista1ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            int a;
+
+            Console.WriteLine("Informe a aresta do quadrado:");
+            l = int.Parse(Console.ReadLine());
+
+            a = l * l;
+
+            Console.WriteLine("A area do quadrado é:");
+            Console.WriteLine(a);
+           
+        }
+    }
+}
