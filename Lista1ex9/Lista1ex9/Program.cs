101250d2fe1b9b9d43fb8543a9f65d8f0e9caa2c
Guimascena <gmascena.odm@gmail.com>
2026-04-11 13:08:42 -0300

    Add files via upload

---
+++ Lista1ex9/Lista1ex9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diametro = 0, raio, area;
+
+            Console.WriteLine("Digite o diâmetro do círciulo:");
+            diametro = double.Parse(Console.ReadLine());
+
+            raio = diametro / 2;
+
+            area = Math.PI * Math.Pow(raio, 2);
+
+            Console.WriteLine($"Área do círculo: {area}");
+
+        }
+    }
+}
