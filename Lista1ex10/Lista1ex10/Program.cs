d4d748f02f474edf2ce3337d533443afc67ca0fa
Guimascena <gmascena.odm@gmail.com>
2026-04-11 13:23:12 -0300

    Add files via upload

---
+++ Lista1ex10/Lista1ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacao = 0, dolares = 0, reais;
+
+            Console.WriteLine("Digite a cotação do dólar:");
+            cotacao = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de dólares:");
+            dolares = double.Parse(Console.ReadLine());
+
+            reais = cotacao * dolares;
+
+            Console.WriteLine($"Valor em reais: R$ {reais}");
+
+        }
+    }
+}
