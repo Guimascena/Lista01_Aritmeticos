19ce12ffe15e1a7719c466f40dc9356af11b6b5c
Guimascena <gmascena.odm@gmail.com>
2026-04-11 12:03:19 -0300

    Add files via upload

---
+++ Lista1ex6/Lista1ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m1 = 0, m2 = 0, media;
+
+            Console.WriteLine("Digite o primeiro valor:");
+            m1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            m2 = double.Parse(Console.ReadLine());
+
+            media = Math.Sqrt(m1 * m2);
+
+            Console.WriteLine($"A média geométrica é: {media}");
+        }
+    }
+}
