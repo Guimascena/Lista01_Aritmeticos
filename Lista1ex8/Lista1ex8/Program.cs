b52115d20bf9b58df9beae24d0ee63631e629e8c
Guimascena <gmascena.odm@gmail.com>
2026-04-11 12:51:53 -0300

    Add files via upload

---
+++ Lista1ex8/Lista1ex8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = 0, f;
+
+            Console.WriteLine("Digite a temperatura em Celsius:");
+            c = double.Parse(Console.ReadLine());
+
+            f = (9.0 / 5.0) * c + 32;
+
+            Console.WriteLine($"Temperatura em Fahrenheit: {f}");
+
+        }
+    }
+}
