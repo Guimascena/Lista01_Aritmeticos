a4959b082f3a2f03f6d728fe9088a75607b0d8cb
Guimascena <gmascena.odm@gmail.com>
2026-04-11 12:29:55 -0300

    Add files via upload

---
+++ Lista1ex7/Lista1ex7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas = 0, km;
+
+            Console.WriteLine("Digite a quantidade em milhas marítimas:");
+            milhas = double.Parse(Console.ReadLine());
+
+            km = milhas * 1.852;
+
+            Console.WriteLine($"Em quilômetros: {km}");
+        }
+    }
+}
