18d178e9051db4e751f0bc3d2584d144a1be461f
Guimascena <gmascena.odm@gmail.com>
2026-04-12 01:25:12 -0300

    Add files via upload

---
+++ Lista1ex11/Lista1ex11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0, resultado;
+
+            Console.WriteLine("Digite o valor de X:");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de Y:");
+            y = double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(x, y);
+
+            Console.WriteLine($"Resultado de X elevado a Y: {resultado}");
+
+        }
+    }
+}
